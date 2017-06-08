6dc24488d7365d361c3094f354d2b25395d011dc
Rick Teachey <ricky@teachey.org>
2017-06-08 01:23:19 -0400

    Added unformat file module
    Unformat file module can read a file validated by a series of  parmatters or format groups.

--- src/parmatter/__init__.py
+++ src/parmatter/__init__.py
@@ -1,3 +1,4 @@
 from .parmatter import Formatter, Parmatter
 from .parmatters import StaticParmatter, DefaultParmatter, AttrParmatter, PositionalDefaultParmatter, KeywordParmatter, VersatileParmatter
 from .group import FormatGroup, FormatGroupMeta
+from .unformat_file import unformat_file
---
+++ src/parmatter/unformat_file.py
@@ -0,0 +1,40 @@
+from collections import namedtuple as nt
+
+UnformatFile = nt('UnformatFile', 'struct result')
+
+def unformat_file(path, line_rules):
+    '''Builds the LineType sequence and LineType.unformat result for a file
+    line_rules: defines valid LineType succession. a dict of the form:
+        parse.compile obj: (parse.compile obj, parse.compile obj, ...)
+        use None for the first line
+    raises TypeError if an invalid line sequence is encountered'''
+    with open(path) as f:
+        lines = list(f)
+        
+    file_struct=[]
+    file_items=[]
+
+    for i,line in enumerate(lines):
+        # skip blank lines
+        if line.strip():
+            try:
+                PrevType=file_struct[-1]
+            except IndexError:
+                # first line
+                PrevType=None
+            for LineType in line_rules[PrevType]:
+                unformat=LineType.unformat(line)
+                if unformat is None:
+                    # format not matched
+                    continue
+                else:
+                    file_items.append(unformat)
+                    file_struct.append(LineType)
+                    break
+                raise TypeError('Failed to read {} at line #{:d}:\n{!r}"'.format(path.name,i+1,line))
+        else:
+            file_items.append(None)
+            file_struct.append(None)
+            
+    assert len(file_struct)==len(file_items)
+    return UnformatFile(file_struct, file_items)
